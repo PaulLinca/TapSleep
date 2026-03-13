6c369c3a9e231342906175edd0b60acb51ae181f
Linca, Paul <paul.linca@sky.uk>
2026-03-13 10:31:36 +0100

    TAPSLEEP | Release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.linca.tapsleep.android"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
     buildFeatures {
         compose = true
